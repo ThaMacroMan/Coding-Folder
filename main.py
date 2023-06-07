f25394d3cdf0b988e5134b749f7bd755689c4474
JOSH KUSKI <joshkuski@gmail.com>
2023-06-07 10:45:09 -0600

    runtime

--- main.py
+++ main.py
@@ -6,8 +6,14 @@ main = Flask(__name__)
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    main.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    import os
+    main.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+
+
+#if __name__ == '__main__':
+#    main.run(host='0.0.0.0', port=8080)
 
 
 
